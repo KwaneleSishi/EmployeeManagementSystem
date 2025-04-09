aad7269f0ec149b3aac018dcad8786807d548116
KwaneleSishi <kwanelesishi050509@gmail.com>
2025-04-09 12:46:45 +0200

    Add project files.

---
+++ EmployeeData.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace EmployeeManagementSystem
+{
+    class EmployeeData
+    {
+
+         // public string EmployeeID { set; get; } // 1
+        public string ID { set; get; }          // 0: id
+        public string Name { set; get; }      // 1: full_name
+        public string Gender { set; get; }    // 2: gender
+        public string Contact { set; get; }   // 3: contact_number
+        public string Position { set; get; }  // 4: position
+        public string Image { set; get; }     // 5: image
+        public decimal Salary { set; get; }       // 6: salary
+        public string Status { set; get; }    // 7: status
+
+
+        SqlConnection connect = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\Documents\employee.mdf;Integrated Security=True;Connect Timeout=30");
+
+
+        public List<EmployeeData> employeeListData()
+        {
+            List<EmployeeData> listdata = new List<EmployeeData>();
+
+            if (connect.State != ConnectionState.Open)
+            {
+                try
+                {
+                    connect.Open();
+
+                    string selectData = "SELECT id, full_name, gender, contact_number, position, image, salary, status " +
+                    "FROM employees WHERE delete_date IS NULL";
+
+                    using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                    {
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            EmployeeData ed = new EmployeeData();
+                            ed.ID = reader["id"].ToString();
+                            // ed.EmployeeID = reader["employee_id"].ToString();
+                            ed.Name = reader["full_name"].ToString();
+                            ed.Gender = reader["gender"].ToString();
+                            ed.Contact = reader["contact_number"].ToString();
+                            ed.Position = reader["position"].ToString();
+                            ed.Image = reader["image"].ToString();
+                            ed.Salary = Convert.ToDecimal(reader["salary"]);
+                            ed.Status = reader["status"].ToString();
+
+                            listdata.Add(ed);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading data: " + ex.Message, "Error");
+                    // Log the error: File.AppendAllText("log.txt", ex.ToString());
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+            return listdata;
+        }
+
+        public List<EmployeeData> salaryEmployeeListData()
+        {
+            List<EmployeeData> listdata = new List<EmployeeData>();
+
+            if (connect.State != ConnectionState.Open)
+            {
+                try
+                {
+                    connect.Open();
+
+                    string selectData = "SELECT * FROM employees WHERE delete_date IS NULL";
+
+                    using (SqlCommand cmd = new SqlCommand(selectData, connect))
+                    {
+                        SqlDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            EmployeeData ed = new EmployeeData();
+                            // ed.EmployeeID = reader["employee_id"].ToString();
+                            ed.ID = reader["id"].ToString();
+                            ed.Name = reader["full_name"].ToString();
+                            ed.Position = reader["position"].ToString();
+                            ed.Salary = Convert.ToDecimal(reader["salary"]);
+
+                            listdata.Add(ed);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex);
+                }
+                finally
+                {
+                    connect.Close();
+                }
+            }
+            return listdata;
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE users
+(
+	id INT PRIMARY KEY IDENTITY(1,1),
+	username VARCHAR(MAX) NULL,
+	password VARCHAR(MAX) NULL,
+	date_register DATE NULL
+)
+
+SELECT * FROM users
+
+CREATE TABLE employees (
+    employee_id VARCHAR(50) PRIMARY KEY,
+    full_name VARCHAR(100)  NULL,
+    gender VARCHAR(10)  NULL,
+    contact_number VARCHAR(20) NULL,
+    position VARCHAR(50)  NULL,
+    image VARCHAR(255)  NULL,
+    salary DECIMAL(10, 2) NULL,
+    insert_date DATE  NULL,
+    update_date DATE NULL,
+    delete_date DATE,
+    status VARCHAR(MAX) NULL
+)
+
+SELECT * FROM employees
+
+SELECT COLUMN_NAME 
+FROM INFORMATION_SCHEMA.COLUMNS 
+WHERE TABLE_NAME = 'employees';
+
